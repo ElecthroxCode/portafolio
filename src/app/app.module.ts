329c06004f9d538db284c8b9a9f317ea84ca2739
Emmanuel <emmanuelsilano@gmail.com>
2024-10-27 23:36:51 -0500

    portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
+import { MaterialModule } from './mdodules/material/material.module';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
